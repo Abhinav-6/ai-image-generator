fa49d60199c0946dac6b269443b286ede62522ae
Abhinav Kumar <abhinavkumar34002@gmail.com>
2023-01-25 08:06:56 +0530

    Added dalle route and bodyparser

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
 import express from "express";
 import * as dotenv from "dotenv"
-import cors from "cors"
+import cors from "cors";
+import bodyParser from "body-parser";
+
+import dalleRoute from "./routes/dalle.js";
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors({ credentials: true, origin: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   res.send("Namaste World");
 })
 
+app.use("/v1/create", dalleRoute);
 
 const startServer = () => {
   app.listen(3001, () => {
---
+++ server/routes/dalle.js
@@ -0,0 +1,36 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const dalleRoute = express.Router();
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(config);
+
+dalleRoute.get("/", (req, res) => {
+  res.status(200).send("Hello from Dalle");
+});
+
+dalleRoute.post("/", async (req, res) => {
+  const { prompt } = req.body;
+
+  try {
+    const response = await openai.createImage({
+      prompt,
+      n: 2,
+      size: "1024x1024",
+    });
+    console.log(response);
+    res.json(response.data.data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+export default dalleRoute;
